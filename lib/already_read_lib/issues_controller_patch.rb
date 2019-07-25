8b336d76ccca3a486efcbd5668642a947b901fe4
Владимир Ковалев <KovalevVV3@it.mos.ru>
2019-07-25 16:49:27 +0300

    bug on bulk update issues

--- lib/already_read_lib/issues_controller_patch.rb
+++ lib/already_read_lib/issues_controller_patch.rb
@@ -4,23 +4,27 @@ module AlreadyReadLib
       base.send :include, InstanceMethods
 
       base.class_eval do
-        # Overide Controller's authorize filter, exclude bulk_set_read
-        before_filter :authorize, :except => [:index, :bulk_set_read]
-        after_filter :issue_read, :only => [:show]
-        after_filter :bulk_set_read, :only => [:bulk_edit, :bulk_update, :destroy]
+        before_filter :authorize, :except => :bulk_set_read
+        after_filter :issue_read, :only => :show
+        after_filter :set_read, :only => :bulk_update
       end
     end
 
     module InstanceMethods
+
+      def set_read
+        issues = Issue.where(:id => params["ids"]);
+        User.current.already_read_issues << issues.reject{|issue| issue.already_read?}
+      end
+
       def bulk_set_read
         issues = Issue.where(:id => params["ids"]);
-        Rails::logger.info "AlreadyRead Plugin: bulk_set_read:  #{params}, issues: #{issues}" if Rails::logger && Rails::logger.info              
         if params[:set_unread]
-              AlreadyRead.destroy_all(:issue_id => params[:ids], :user_id => User.current.id)
-          else
-              User.current.already_read_issues << issues.reject{|issue| issue.already_read?}
-          end
-          redirect_back_or_default({:controller => 'issues', :action => 'index', :project_id => @project})
+          AlreadyRead.destroy_all(:issue_id => params[:ids], :user_id => User.current.id)
+        else
+          User.current.already_read_issues << issues.reject{|issue| issue.already_read?}
+        end
+        redirect_back_or_default({:controller => 'issues', :action => 'index', :project_id => @project})
       end
 
       private
